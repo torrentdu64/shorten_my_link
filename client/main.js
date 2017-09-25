2d9a9633a57b4639fa7a1cdd71b8186475d36cdf
torrent benoit <torrentdu64@gmail.com>
2017-09-25 16:00:33 +0200

    add tree folder project

---
+++ client/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const App = () => {
+  return (
+    <h1>hello</h1>
+    );
+};
+
+
+Meteor.startup( () => {
+  ReactDOM.render(<App />, document.querySelector('.render-target'));
+});
