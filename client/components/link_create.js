2facafd168afc873f66a7133ba4514ea1dca6fa7
torrent benoit <torrentdu64@gmail.com>
2017-09-26 16:25:24 +0200

    display error message

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -3,20 +3,35 @@ import React, {Component} from 'react';
 
 
 class LinkCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { error: '' };
+  }
 
   handleSubmit(event) {
       event.preventDefault();
-      Meteor.call('links.insert', this.ref.link.value);
+
+    Meteor.call('links.insert', this.refs.link.value, (error) => {
+        if(error) {
+          this.setState({ error: 'url invalide'});
+        } else {
+            this.setState({ error: ''});
+            this.refs.link.value = '';
+        }
+
+      });
   }
 
 
   render(){
     return(
-      <form onSubmit={this.handleSubmit.bin(this)}>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <label>Link short</label>
             <input ref="link" className="form-control" />
         </div>
+        <div className="text-danger" > {this.state.error}</div>
         <button  className="btn btn-primary">Shorten</button>
       </form>
       );
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,10 +1,11 @@
 import { Mongo } from 'meteor/mongo';
 import validURL from 'valid-url';
-import { check, Match} from 'meteor/meteor';
+import { check, Match} from 'meteor/check';
 
 Meteor.methods({
-  'links.insert' : function (url){
-      validURL.isUri(url);
+  'links.insert': function (url){
+
+      check(url, Match.Where(url => validURL.isUri(url)));
   }
 
 });
