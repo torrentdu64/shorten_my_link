0af1b8e18a922b1d23562e74184c51c1b22951b8
torrent benoit <torrentdu64@gmail.com>
2017-09-27 14:50:42 +0200

    add middleware to redirect with token url

--- server/main.js
+++ server/main.js
@@ -1,8 +1,18 @@
 import { Meteor } from 'meteor/meteor';
 import {Links} from '../imports/collections/links';
+import {WebApp} from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
 
 Meteor.startup(() => {
   Meteor.publish('links', function() {
       return Links.find({});
   });
 });
+
+    const middleware  = ConnectRoute(function(router){
+          router.get('/:token')
+    });
+
+
+  WebApp.connectHandlers
+    .user(middleware)
